a7637643f7923c46711b54f02fad99e00827d2c1
Rajarishi <rishi_devan@mail.com>
2017-09-30 18:04:31 +0530

    changed allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5q!+#-^ovu+ky+_b46*6ado#tm_nxobc)=$1h&66c#lu0()h=3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rishisnotes.pythonanywhere.com', 'localhost']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
