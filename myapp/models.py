be55b8a667b611a821a96312b5fcec0cf91fcc05
Rajarishi <rishi_devan@mail.com>
2017-09-30 23:15:00 +0530

    Created model Question

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.TextField()
+    created_at = models.DateTimeField(default = timezone.now())
+    author = models.ForeignKey('auth.User')
+
+    def __str__(self):
+        return self.question_text
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 import datetime
+from .models import Question
 
 # Create your views here.
 def home(request):
+	questions = Question.objects.all()
 	today_datetime = datetime.datetime.now()
-	return render(request, 'home.html', { 'time': today_datetime })
+	return render(request, 'home.html', { 'time': today_datetime, 'questions': questions })
 
 def page(request, id):
 	return render(request, 'page.html', { 'id': id })
