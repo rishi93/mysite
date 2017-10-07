9b77bec377b9e1e79cd07350f010dd6702c1b560
Rajarishi <rishi_devan@mail.com>
2017-10-07 11:56:47 +0530

    changed to postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,11 +77,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'wcrqosbr',
+        'PASSWORD': 'cntwMvYqDdptKxaJpaVNy_mvkeE7xVh0',
     }
 }
 
+DATABASES['default'] = dj_database_url.config(default="postgres://wcrqosbr:cntwMvYqDdptKxaJpaVNy_mvkeE7xVh0@pellefant.db.elephantsql.com:5432/wcrqosbr")
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
